ecbec63cde84c8fdf77d3da9d2dda87a830b425c
riley <mechamriley@gmail.com>
2024-09-28 19:58:35 -0600

    updated the generateMarkdwon function

---
+++ Develop/README.md
@@ -0,0 +1,25 @@
+# Profession ReadME.md 
+## Description 
+A CLI tool to help developers create professional readme.md files in less time.
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Github](#github)
+* [Email](#email)
+
+* [License](#license)
+
+## Installation
+This tool uses inpuirer. Make sure to run npm i.
+## Usage
+To initate the tool run the file in node.js using node index.js.
+## Contribution
+Use GitHub to contribute
+## License
+This project is licensed under the MIT license.
+![License](https://img.shields.io/badge/License-MIT-yellow.svg)
+## GitHub
+riley6639
+## Email
+mechamriley@gmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return license ? `![License](https://img.shields.io/badge/License-${license}-blue.svg)` : '';
+  return license ? `![License](https://img.shields.io/badge/License-${license}-yellow.svg)` : '';
 }
 
 // TODO: Create a function that returns the license link
@@ -19,14 +19,27 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-##${renderLicenseBadge(data.title)}
-##${renderLicenseLink(data.description)}
-##${renderLicenseSection(data.installation)}
-##${renderLicenseSection(data.usage)}
-##${renderLicenseSection(data.contribution)}
-##${renderLicenseSection(data.license)}
-##${renderLicenseSection(data.github)}
-##${renderLicenseSection(data.email)}
+## Description 
+${data.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Github](#github)
+* [Email](#email)
+${renderLicenseLink(data.license)}
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Contribution
+${data.contribution}
+${renderLicenseSection(data.license)}
+${renderLicenseBadge(data.license)}
+## GitHub
+${data.github}
+## Email
+${data.email}
 `;
 }
 
